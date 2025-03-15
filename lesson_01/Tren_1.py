6d6489d9a7f9161632731855bb62b45417a8fd0f
Sky-Vik <testing.vk24@gmail.com>
2025-03-15 18:03:12 +0300

    ДЗ Урок 1 Python

---
+++ lesson_01/Tren_1.py
@@ -0,0 +1,25 @@
+my_heigh=156
+print(my_heigh)
+
+my_name="Виктория"
+my_name="Виктория Кекконен"
+print("Меня зовут", my_name)
+
+pet_name = input("Как зовут вашего питомца?")
+print("Ваш любимец -" + pet_name)
+
+def print_python():
+    print("Учу Python!")
+
+print_python()
+
+def print_letter(let):
+    print(let, end='')
+
+print_letter('С')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
---
+++ lesson_01/lesson_1_task_5.py
@@ -0,0 +1,14 @@
+def print_tel(num):
+    print(num, end = '')
+# 88005553535
+print_tel(8)
+print_tel(8)
+print_tel(0)
+print_tel(0)
+print_tel(5)
+print_tel(5)
+print_tel(5)
+print_tel(3)
+print_tel(5)
+print_tel(3)
+print_tel(5)
